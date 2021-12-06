452d0ab7aca170b514cb5c8ec260ce6472674941
Adrian Kiva <akiva@mta.ca>
2021-12-06 00:49:49 -0500

    finish 1 start 2

--- 06.js
+++ 06.js
@@ -11,6 +11,32 @@ fs.readFile('./data/06-input.txt', 'utf-8', (err, data) => {
   const regex1 = /\w+/g
 
   let dataArray = 
-  data.split('\r\n')
+  data
+  .split(',')
+  .map(a => Number.parseInt(a))
 
+  const days = 80;
+  let totalFish = 0;
+
+  const fishDay = (remainingDays, fish) => {
+    if (remainingDays > fish) {
+      return fishDay(remainingDays - fish, 7) + fishDay(remainingDays - fish, 9)
+    } return 1
+  }
+
+  for (let fish of dataArray) {
+      totalFish += fishDay(days, fish)
+    }
+
+    // dataArray = dataArray.map((a, index, array) => {
+    //   if (a === 0) {
+    //     array = array.push(8);
+    //     return 6
+    //   } return (a - 1)
+    // })
+    // console.log(dataArray)
+  // console.log(dataArray);
+
+
+  console.log(totalFish);
 });
