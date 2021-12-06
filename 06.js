a7ac4f18d58f9f3bd3fea6181d15413f1cc3b326
Adrian Kiva <akiva@mta.ca>
2021-12-06 10:29:28 -0500

    refactor part 1 to match part 2

--- 06.js
+++ 06.js
@@ -16,19 +16,39 @@ fs.readFile('./data/06-input.txt', 'utf-8', (err, data) => {
   .map(a => Number.parseInt(a))
 
   const days = 80;
-  let totalFish = 0;
+  const birthGestationAge = 8;
+  const gestationAge = 6;
+  let twoDimensionalFish = [];
 
-  const fishDay = (remainingDays, fish) => {
-    if (remainingDays > fish) {
-      return fishDay(remainingDays - fish, 7) + fishDay(remainingDays - fish, 9)
-    } return 1
+  const howManyAge = (array) => {
+    for (let i = 0; i <= birthGestationAge; i ++) {
+      let age = i;
+      let number = array.filter(a => a === age).length
+      twoDimensionalFish = twoDimensionalFish.concat(number)
+    }
+  }
+  howManyAge(dataArray);
+
+  //more hard coding here than i would like
+  const oneDayAtATime = (array) => {
+    let newArr = array;
+    for (let i = 0; i < days; i ++) {
+      newArr = [
+        newArr[1],
+        newArr[2],
+        newArr[3],
+        newArr[4],
+        newArr[5],
+        newArr[6],
+        newArr[7] + newArr[0],
+        newArr[8],
+        newArr[0],
+      ];
+    }
+    console.log('array: ', newArr)
+    console.log('total: ', newArr.reduce((sum, a) => sum + a))
   }
 
-  for (let fish of dataArray) {
-      console.log(totalFish)
-      totalFish += fishDay(days, fish)
-    }
+console.log(oneDayAtATime(twoDimensionalFish))
 
-
-  console.log(totalFish);
 });
