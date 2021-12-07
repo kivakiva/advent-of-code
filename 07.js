0384648b190bae79e326123c88aa1c439bb68007
Adrian Kiva <akiva@mta.ca>
2021-12-07 01:38:27 -0500

    part 2

--- 07.js
+++ 07.js
@@ -13,8 +13,6 @@ data
 .split(',')
 .map(a => Number.parseInt(a))
 
-
-
 const max = dataArray.reduce((max, a) => {
   if (a > max) {return a}
   return max
@@ -24,13 +22,8 @@ let minPos = {
   min: max * dataArray.length,
   position: 0
 };
-let fueltest = dataArray.reduce((sum, a) => {
-  return sum + Math.abs(a - 3)
-},0);
-console.log('test at 3: ',fueltest)
 
-
-for (let i = max * 2; i > - max; i--) {
+for (let i = 1; i <=max; i++) {
 
   //find fuel 
   let fuel = dataArray.reduce((sum, a) => {
@@ -41,17 +34,11 @@ for (let i = max * 2; i > - max; i--) {
     minPos.min = fuel;
     minPos.position = i;
   }
-  if (i % 30 === 0) {console.log('fuel:  ', fuel);console.log('best pos: ', minPos.position);console.log('best solution:  ', minPos.min)}
 }
 
-// const average = dataArray.reduce((sum, a) => sum + a) / dataArray.length;
-
-// const middle = Math.floor(dataArray.length / 2)
-console.log('data array length', dataArray.length)
-console.log('max: ', max)
 console.log('least fuel:  ', minPos.min)
 console.log('position of least fuel:  ', minPos.position)
 console.log(dataArray.slice(0,20))
 console.log(dataArray.slice(980,1000))
 // 332 (median) is incorrect
-//330 is incorrect
+//330 is incorrect (misread the problem input)
--- 07b.js
+++ 07b.js
@@ -1,5 +1,6 @@
 /* description
-
+-make all the numbers equal by changing them as little as possible
+-return sum of change magnitude
 logic
 */
 
@@ -7,13 +8,43 @@ const fs = require('fs')
 
 data = fs.readFileSync('./data/07-input.txt', 'utf-8')
 
-const regex1 = /\w+/g
-
 let dataArray = 
 data
 .split(',')
 .map(a => Number.parseInt(a))
 
 
+const factorial = (number) => {
+  if (number === 0) {return 0}
+  else if (number === 1) {return 1}
+  else {return number + factorial(number - 1)}
+}
 
-console.log(dataArray)
+const max = dataArray.reduce((max, a) => {
+  if (a > max) {return a}
+  return max
+});
+
+let minPos = {
+  min: factorial(max) * dataArray.length,
+  position: 0
+};
+
+
+for (let i = 1; i <= max; i++) {
+
+  //find fuel 
+  let fuel = dataArray.reduce((sum, a) => {
+    return sum + factorial(Math.abs(a - i))
+  },0);
+
+  if (minPos.min > fuel) {
+    minPos.min = fuel;
+    minPos.position = i;
+  }
+}
+
+console.log('least fuel:  ', minPos.min)
+console.log('position of least fuel:  ', minPos.position)
+
+//1934000 at zero is incorrect (did not make defualt min high enough)
